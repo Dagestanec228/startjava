74d0cc4f98f1f919844e4364335898f1106e4f7b
Dan <lucker2009@mail.ru>
2020-07-25 13:37:05 +0300

    a

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {	
+    public static void main(String[] args) {
+
+        byte CoreCount = 4;
+        short RAM = 16384;
+        int CpuFreqMHz = 3200;
+        long MaxRAM = 65536;
+        float BiosVersion = 2.314F;
+        double HDDfree = 375.4;
+        char CpuVersion = 'K';
+        boolean HyperThreadingTech = true;
+
+        System.out.println("CoreCount = " + CoreCount);
+        System.out.println("RAM = " + RAM);
+        System.out.println("CpuFreqMHz = " + CpuFreqMHz);
+        System.out.println("MaxRAM = " + MaxRAM);
+        System.out.println("BiosVersion = " + BiosVersion);
+        System.out.println("HDDfree = " + HDDfree);
+        System.out.println("CpuVersion = " + CpuVersion);
+        System.out.println("HyperThreadingTech = " + HyperThreadingTech);
+    }
+}
